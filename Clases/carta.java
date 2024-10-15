bc61620bc82b3324d48b7247327eff8469443acf
Fer03Liz <fjlizarazog@gmail.com>
2024-10-14 20:21:19 -0500

    Creacion interfaz primera entrega

--- Clases/carta.java
+++ Clases/carta.java
@@ -1,36 +1,87 @@
 public class carta {
-    private String color;
-    private Integer valor;
-    private String accion;
+    private String color1;
+    private String color2;
+    private Integer valor1;
+    private Integer valor2;
+    private String accion1;
+    private String accion2;
+    private int lado1; // 0 para tradicional, 1 para lado oscuro (FLIP)
+    private int lado2; // 0 para tradicional, 1 para lado oscuro (FLIP)
 
-    public carta(String color, Integer valor, String accion) {
-        this.accion = accion;
-        this.color = color;
-        this.valor = valor;
+    public carta(String color1, String color2, Integer valor1, Integer valor2, String accion1, String accion2,
+            int lado1, int lado2) {
+        this.accion1 = accion1;
+        this.color1 = color1;
+        this.valor1 = valor1;
+        this.lado1 = lado1;
+        this.accion2 = accion2;
+        this.color2 = color2;
+        this.valor2 = valor2;
+        this.lado2 = lado2;
     }
 
-    public String getAccion() {
-        return accion;
+    public int getLado2() {
+        return lado2;
     }
 
-    public void setAccion(String accion) {
-        this.accion = accion;
+    public void setLado2(int lado) {
+        this.lado2 = lado;
     }
 
-    public Integer getValor() {
-        return valor;
+    public int getLado1() {
+        return lado1;
     }
 
-    public void setValor(Integer valor) {
-        this.valor = valor;
+    public void setLado1(int lado) {
+        this.lado1 = lado;
     }
 
-    public String getColor() {
-        return color;
+    public String getAccion1() {
+        return accion1;
     }
 
-    public void setColor(String color) {
-        this.color = color;
+    public void setAccion1(String accion) {
+        this.accion1 = accion;
+    }
+
+    public String getAccion2() {
+        return accion2;
+    }
+
+    public void setAccion2(String accion) {
+        this.accion2 = accion;
+    }
+
+    public Integer getValor1() {
+        return valor1;
+    }
+
+    public void setValor1(Integer valor) {
+        this.valor1 = valor;
+    }
+
+    public Integer getValor2() {
+        return valor2;
+    }
+
+    public void setValor2(Integer valor) {
+        this.valor2 = valor;
+    }
+
+    public String getColor1() {
+        return color1;
+    }
+
+    public void setColor1(String color) {
+        this.color1 = color;
+    }
+
+    public String getColor2() {
+        return color2;
+    }
+
+    public void setColor2(String color) {
+        this.color2 = color;
     }
 
 }
--- Clases/juego.java
+++ Clases/juego.java
@@ -1,15 +1,38 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class juego {
 
     private int numeroJugadores;
 
-    private mazo cartasCaja;
+    private int numJugadoresReales;
 
-    public int getNumeroJugadores() {
-        return numeroJugadores;
+    private int numJugadoresSinteticos;
+
+    public int getNumJugadoresReales() {
+        return numJugadoresReales;
     }
 
-    public void setNumeroJugadores(int numeroJugadores) {
-        this.numeroJugadores = numeroJugadores;
+    public void setNumJugadoresReales(int numJugadoresReales) {
+        this.numJugadoresReales = numJugadoresReales;
+    }
+
+    public int getNumJugadoresSinteticos() {
+        return numJugadoresSinteticos;
+    }
+
+    public void setNumJugadoresSinteticos(int numJugadoresSinteticos) {
+        this.numJugadoresSinteticos = numJugadoresSinteticos;
+    }
+
+    private mazo cartasCaja;
+
+    public void iniciarJuego() {
+
+    }
+
+    public void terminarJuego() {
+
     }
 
     public juego() {
@@ -17,8 +40,69 @@ public class juego {
     }
 
     public static void main(String[] args) {
-        System.err.println("HOLA MUNDO  ");
+        System.out.println(
+                "  *********************         ********************************************         *********************");
+        System.out.println("                                *                                          * ");
+        System.out.println("                                *   B   I   E   N   V   E   N   I   D   O  * ");
+        System.out.println("                                *                                          * ");
+        System.out.println("                                *                                          * ");
+        System.out.println(
+                "  *********************         ********************************************         *********************");
 
+        juego juego = new juego();
+        try (Scanner scanner = new Scanner(System.in)) {
+            juego.setNumeroJugadores(0);
+            int numJugadores;
+            while (true) {
+                try {
+                    System.out.print("¿Cuántos jugadores van a participar? (entre 2 y 10): ");
+                    numJugadores = scanner.nextInt();
+
+                    if (numJugadores < 2 || numJugadores > 10) {
+                        System.out.println("Error: el número de jugadores debe ser entre 2 y 10. Inténtalo de nuevo.");
+                    } else {
+                        break;
+                    }
+                } catch (InputMismatchException e) {
+                    System.out.println("Error: debe introducir un número. Inténtalo de nuevo.");
+                    scanner.next();
+                }
+            }
+            juego.setNumeroJugadores(numJugadores);
+            int numReales = 0;
+            while (true) {
+                try {
+                    System.out.print(
+                            "¿Cuántos jugadores serán reales? (entre 0 y " + juego.getNumeroJugadores() + "): ");
+                    numReales = scanner.nextInt();
+
+                    if (numReales < 0 || numReales > juego.getNumeroJugadores()) {
+                        System.out.println("Error: el número de jugadores sintéticos debe ser entre 0 y "
+                                + juego.getNumeroJugadores() + ". Inténtalo de nuevo.");
+                    } else {
+                        break;
+                    }
+                } catch (InputMismatchException e) {
+                    System.out.println("Error: debe introducir un número. Inténtalo de nuevo.");
+                    scanner.next();
+                }
+            }
+
+            jugador[] jugadores = new jugador[numReales];
+
+            for (int i = 0; i < numReales; i++) {
+                System.out.print("Introduce el nombre del jugador " + (i + 1) + ": ");
+                String nombre = scanner.next();
+
+                jugadores[i] = new jugador(nombre);
+                System.out.println("Jugador " + (i + 1) + " creado: " + jugadores[i].getNombre());
+            }
+            juego.setNumJugadoresReales(numReales);
+            juego.setNumJugadoresSinteticos(numJugadores - numReales);
+            System.out.println("¡Genial! El juego comenzará con " + numJugadores + " jugadores. ("
+                    + juego.getNumJugadoresReales() + " son jugadores reales y " + juego.getNumJugadoresSinteticos()
+                    + " son jugadores sinteticos)");
+        }
     }
 
     public mazo getCartasCaja() {
@@ -27,5 +111,13 @@ public class juego {
 
     public void setCartasCaja(mazo cartasCaja) {
         this.cartasCaja = cartasCaja;
+    }
+
+    public int getNumeroJugadores() {
+        return numeroJugadores;
+    }
+
+    public void setNumeroJugadores(int numeroJugadores) {
+        this.numeroJugadores = numeroJugadores;
     }
 }
