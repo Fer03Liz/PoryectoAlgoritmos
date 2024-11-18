89a9ccd84f3d7aa5e08106df2c3d7446a1c63a22
Fer03Liz <fjlizarazog@gmail.com>
2024-11-17 23:25:07 -0500

    Correccion de los colores

--- Clases/juego.java
+++ Clases/juego.java
@@ -5,11 +5,13 @@ import java.util.Scanner;
 public class Juego {
 
     private Baraja baraja;
+    private Baraja mazoDescarte;
     private List<Jugador> jugadores;
     private static final int CARTAS_INICIALES = 7; // Número de cartas iniciales por jugador
 
     public Juego() {
         this.baraja = new Baraja();
+        this.mazoDescarte = new Baraja();
         this.jugadores = new ArrayList<>();
     }
 
@@ -55,11 +57,20 @@ public class Juego {
         // Repartir cartas iniciales
         repartirCartasIniciales();
 
-        // Mostrar jugadores y sus manos
+        // Mostrar jugadores y sus manos iniciales
         System.out.println("\nJugadores y sus manos iniciales:");
         for (Jugador jugador : jugadores) {
             System.out.println("Jugador: " + jugador.getNombre());
             jugador.getCartas().mostrarMazo();
+        }
+
+        // Colocar la primera carta en el mazo de descarte
+        Carta primeraCarta = baraja.tomarCarta();
+        if (primeraCarta != null) {
+            mazoDescarte.agregarCarta(primeraCarta);
+            System.out.println("\nCarta inicial en el mazo de descarte: " 
+                    + primeraCarta.getColorActual() + " " 
+                    + (primeraCarta.getAccionActual() != null ? primeraCarta.getAccionActual() : primeraCarta.getValorActual()));
         }
 
         System.out.println("\n¡El juego está listo para comenzar!");
@@ -72,40 +83,39 @@ public class Juego {
         String[] coloresLadoOscuro = {"Rosa", "Turquesa", "Morado", "Naranja"};
         String[] accionesLadoClaro = {"Saltar", "Reversa", "+2"};
         String[] accionesLadoOscuro = {"Saltar", "Reversa", "+5"};
-    
+
         // Generar cartas numéricas (0-9)
         for (int i = 0; i < coloresLadoClaro.length; i++) {
             for (int valor = 0; valor <= 9; valor++) {
                 baraja.agregarCarta(new Carta(coloresLadoClaro[i], valor, null, coloresLadoOscuro[i], valor, null));
             }
         }
-    
+
         // Generar cartas de acción estándar (Saltar, Reversa, +2)
         for (int i = 0; i < coloresLadoClaro.length; i++) {
             for (String accion : accionesLadoClaro) {
                 baraja.agregarCarta(new Carta(coloresLadoClaro[i], -1, accion, coloresLadoOscuro[i], -1, null));
             }
         }
-    
+
         // Generar cartas de acción avanzadas para el lado oscuro (+5)
         for (int i = 0; i < coloresLadoClaro.length; i++) {
             for (String accion : accionesLadoOscuro) {
                 baraja.agregarCarta(new Carta(coloresLadoClaro[i], -1, null, coloresLadoOscuro[i], -1, accion));
             }
         }
-    
+
         // Generar cartas "Flip" de color Comodín
         for (int i = 0; i < 4; i++) {
             baraja.agregarCarta(new Carta("Comodín", -1, "Flip", "Comodín", -1, "Flip"));
         }
-    
+
         // Generar cartas de comodín y Wild +4
         for (int i = 0; i < 4; i++) {
             baraja.agregarCarta(new Carta("Comodín", -1, "Wild", "Comodín", -1, "Wild"));
             baraja.agregarCarta(new Carta("Comodín", -1, "Wild +4", "Comodín", -1, "Wild +4"));
         }
     }
-    
 
     // Método para repartir cartas iniciales a cada jugador
     private void repartirCartasIniciales() {
